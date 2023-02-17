00c5b29bc102729f3c4d645cac669f8ae297d8b0
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-02-17 23:24:04 +0530

    Added: print to add event listener salary range

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -66,3 +66,27 @@ class EmployeePayroll {
       }
 }
 
+window.addEventListener('DOMContentLoaded', (event) {
+    const name = document.querySelector(#name);
+    const textError = document.querySelector('text-error');
+    name.addEventListener('input', function(){
+        if(name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        }
+        catch (e) {
+            textError.textContent = e;
+        }
+    });
+     const salary = document.querySelector('#salary');
+     const output = document.querySelector('salary-Output');
+     output.textContent = salary.value;
+     salary.addEventListener('input', function() {
+       output.textContent = salary.value;
+    })
+})
+
