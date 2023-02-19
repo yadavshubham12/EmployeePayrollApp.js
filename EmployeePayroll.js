048f46df6c2d2ff8eedce35b4d2830333358c227
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-02-19 12:04:45 +0530

    Added: Save in local storage

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -127,3 +127,25 @@ const getSelectedValues = (propertyValue)=> {
     });
     return selItems;
 }
+
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
+    }
+    catch (e) {
+        return;
+    }
+}
+
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayroll"));
+    if(employeePayrollList != undefined) {
+        employeePayrollList.push(employeePayrollData);
+    }
+    else {
+        employeePayrollList = [employeePayrollData]
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
+}
